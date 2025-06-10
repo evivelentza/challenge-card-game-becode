ca85644b94a8f04ed0dc13e54bf18c614a88370e
Evi Velentza <paraskevivelentza@gmail.com>
2025-06-10 14:57:07 +0200

    player part I lost before

--- utils/player.py
+++ utils/player.py
@@ -1,14 +1,31 @@
 from utils.card import Card
+import random
 
 class Player:
     def __init__(self):
+        self.name = name
         self.cards = []      
         self.turn_count = 0
         self.number_of_cards = 0
         self.history = [] 
 
-player_list = []
+#player_list = []
 
-for i in range(4):
-    player = Player()
-    player_list.append(player)
+#for i in range(4):
+#    player = Player()
+#    player_list.append(player)
+
+def play(self)
+    if not self cards:
+        print(f"{self.name} has no cards left.")
+        return None
+
+    card_to_play = random.choice(self.cards)
+    self.cards.remove(card_to_play)
+
+    self.trun_count += 1
+
+    self.history.append(card_to_play)
+
+    print(f"{self.name} {self.turn_count} played: {card_to_play.value} {card_to_play.icon}")
+    return card_to_play
