87538011e61ac5ceda31c9bd9054fbdae14fa836
Evi Velentza <paraskevivelentza@gmail.com>
2025-06-10 15:26:22 +0200

    new commit

--- utils/player.py
+++ utils/player.py
@@ -2,7 +2,7 @@ from utils.card import Card
 import random
 
 class Player:
-    def __init__(self):
+    def __init__(self, name):
         self.name = name
         self.cards = []      
         self.turn_count = 0
@@ -15,17 +15,20 @@ class Player:
 #    player = Player()
 #    player_list.append(player)
 
-def play(self)
-    if not self cards:
-        print(f"{self.name} has no cards left.")
-        return None
+    def play(self):
 
-    card_to_play = random.choice(self.cards)
-    self.cards.remove(card_to_play)
+        if not self.cards:
+            print(f"{self.name} has no cards left.")
+            return None
 
-    self.trun_count += 1
+        card_to_play = random.choice(self.cards)
+        self.cards.remove(card_to_play)
 
-    self.history.append(card_to_play)
+        self.turn_count += 1
 
-    print(f"{self.name} {self.turn_count} played: {card_to_play.value} {card_to_play.icon}")
-    return card_to_play
+        self.number_of_cards = len(self.cards)
+
+        self.history.append(card_to_play)
+
+        print(f"{self.name} {self.turn_count} played: {card_to_play.value} {card_to_play.icon}")
+        return card_to_play
