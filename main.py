a7163844a4040f42734be81d06a6369ed3b285fb
Evi Velentza <paraskevivelentza@gmail.com>
2025-06-10 15:30:48 +0200

    main.py updated

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 from utils.game import Board
 from utils.player import Player
+
+deck = Deck()
+deck.fill_deck()
+deck.shuffle()
+
+players = []
+for i in range(4):
+    player = Player(name=f"Player {i+1}")
+    players.append(player)
+
+deck.distribute_cards(players)
+
+
+for player in players:
+    print(f"\n{player.name} has {len(player.cards)} cards:")
+    for card in player.cards:
+        print(f" - {card.value} of {card.icon} ({card.color})")
